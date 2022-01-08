0813c7ac595d5c76ee5188cdeeef689ce35cb181
VIKRAM BALAJI <mr.vikram.balaji@gmail.com>
2022-01-08 15:50:18 -0800

    commit on main

--- rainydaymod/src/main/java/deliveried/rainy/day/mixin/ExampleMixin.java
+++ rainydaymod/src/main/java/deliveried/rainy/day/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package deliveried.rainy.day.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import deliveried.rainy.day.RainyDay;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		RainyDay.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
