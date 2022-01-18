9d155cbb355e22f53b4bb427314b9d2ceca42201
VIKRAM BALAJI <mr.vikram.balaji@gmail.com>
2022-01-18 15:09:22 -0800

    chillin

--- rainydaymod/src/main/java/deliveried/rainy/day/RainyDay.java
+++ rainydaymod/src/main/java/deliveried/rainy/day/RainyDay.java
@@ -1,6 +1,7 @@
 package deliveried.rainy.day;
 
 import net.fabricmc.api.ModInitializer;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,7 +10,7 @@ public class RainyDay implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
-
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
